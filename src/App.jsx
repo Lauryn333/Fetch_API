d57b302aac44ed5a101daf9b1f8b1a45835e3f4d
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-05-01 23:24:44 +0200

    Use axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import "./App.css";
 import UserCard from "./components/UserCard";
-
-const user = {
-  firstName: "Kazka",
-  lastName: "Gotra",
-  email: "kazka.gotra@example.com",
-  picture: "https://randomuser.me/api/portraits/women/5.jpg",
-};
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <UserCard user={user} />;
+  const user = {
+    firstName: "Kazka",
+    lastName: "Gotra",
+    email: "kazka.gotra@example.com",
+    picture: "https://randomuser.me/api/portraits/women/5.jpg",
+  };
+
+  const [profil, setProfil] = useState(user);
+
+  const getProfil =
+    (() => {
+      axios
+        .get("https://randomuser.me/api/?inc=name, email, picture")
+        .then((response) => {
+          setProfil(response.data.results[0]);
+        });
+    },
+    []);
+
+  return (
+    <>
+      <UserCard profil={profil} />
+    </>
+  );
 }
 
 export default App;
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,14 +1,13 @@
 import "../scss/userCard.scss";
-function UserCard({ user }) {
+function UserCard({ profil }) {
   return (
     <div className="div-container">
       <div className="user-informations">
-        <img src={user.picture} alt="portrait individuel" />
-
+        <img src={profil.picture} alt="portrait individuel" />
         <h3>
-          {user.firstName} {user.lastName}
+          {profil.firstName} {profil.lastName}
         </h3>
-        <p>{user.email}</p>
+        <p>{profil.email}</p>
       </div>
       <button>Click here to change the user</button>
     </div>
